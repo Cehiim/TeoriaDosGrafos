eebd2c008f144ed313b92ca863c4a359ec3e3419
david-pessoa <pessoa.dvlbp@gmail.com>
2024-09-07 16:32:51 -0300

    corrige linha

--- ListaProfundidadeLargura/TesteDavid.py
+++ ListaProfundidadeLargura/TesteDavid.py
@@ -1,7 +1,7 @@
 from TGrafo import Grafo
 
 grafo = Grafo(8) #Obtém grafo do arquivo .txt
-grafo.leGrafo("./ListaTG/grafo_teste.txt")
+grafo.leGrafo("./ListaProfundidadeLargura/grafo_teste.txt")
 profundidade = grafo.percursoProfindidade(0) #Obtém percurso em largura e profundidade
 largura = grafo.percursoLargura(0)
 
