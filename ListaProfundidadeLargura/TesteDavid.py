fef7da59ad30601e0601ee7177c5eba7ac458a23
david-pessoa <pessoa.dvlbp@gmail.com>
2024-09-07 16:31:43 -0300

    renomeia pasta para ser a lista de profundidade e largura

---
+++ ListaProfundidadeLargura/TesteDavid.py
@@ -0,0 +1,13 @@
+from TGrafo import Grafo
+
+grafo = Grafo(8) #Obtém grafo do arquivo .txt
+grafo.leGrafo("./ListaTG/grafo_teste.txt")
+profundidade = grafo.percursoProfindidade(0) #Obtém percurso em largura e profundidade
+largura = grafo.percursoLargura(0)
+
+for i in range(grafo.n):
+    profundidade[i] = chr(profundidade[i] + 97) # Troca os índices da tabela pelas letras dos vértices
+    largura[i] = chr(largura[i] + 97)
+
+print("Percurso em profundidade:", profundidade) # Imprime os percursos
+print("Percurso em largura:", largura)
--- ListaTG/TesteDallape.py
+++
@@ -1,5 +0,0 @@
-from TGrafo import Grafo
-from TGrafoND import GrafoND
-from TGrafoNDR import GrafoNDR
-from TGrafoR import GrafoR
-
