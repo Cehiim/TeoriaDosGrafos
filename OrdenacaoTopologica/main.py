e3070a1dc0af9d06a90c63b20f046ac9c698875c
david-pessoa <pessoa.dvlbp@gmail.com>
2024-09-14 18:43:25 -0300

    finaliza lab de ordenação topológica

--- OrdenacaoTopologica/main.py
+++ OrdenacaoTopologica/main.py
@@ -5,10 +5,10 @@ grafo = Grafo() #Obtém grafo do arquivo .txt
 ordenacao_topologica = grafo.ordenacaotopologica() #Obtém lista com a ordenação topológica do grafo
 ordenacao = grafo.substitui(ordenacao_topologica) #Substitui os índices dos vértices por letras
 
-print("Lista com a ordenação topológica:", ordenacao) # Imprime a lista
+print("Teste 1: Lista com a ordenação topológica:", ordenacao) # Imprime a lista
 
 # Teste 2
 grafo2 = Grafo("./OrdenacaoTopologica/outro_grafo.txt") #Obtém grafo do arquivo .txt
 topologica2 = grafo2.ordenacaotopologica() #Obtém lista com a ordenação topológica do grafo
 
-print("Lista com a ordenação topológica:", topologica2) # Imprime a lista
+print("Teste 2: Lista com a ordenação topológica:", [num + 1 for num in topologica2]) # Imprime a lista com os números dos vértices
